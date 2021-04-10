018b17541a9a9f5db0f7bfb93d108d477e651535
ryota-kanaumi <ayusan.0729@icloud.com>
2021-04-10 17:48:58 +0900

    issue2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_Week
     @plan = Plan.new
   end
 
@@ -37,6 +37,5 @@ class CalendarsController < ApplicationController
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
-
   end
 end
